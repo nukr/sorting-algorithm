9d22cc517bc7d66f86dcc4c7593585d44b9a2934
Nukr <nukrs.w@gmail.com>
2016-07-04 18:14:55 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// catagory by wikipidia
+// https://en.wikipedia.org/wiki/Sorting_algorithm
+
+// simple sorts
+function insertion_sort (array) { }
+function selection_sort (array) { }
+
+// efficient sorts
+function quick_sort (array) { }
+function merge_sort (array) { }
+function heap_sort (array) { }
+
+// bubble sort and variants
+function bubble_sort (array) { }
+function shell_sort (array) { }
+function comb_sort (array) { }
+
+// distribution sorts
+function counting_sort (array) { }
+function bucket_sort (array) { }
+function radix_sort (array) { }
